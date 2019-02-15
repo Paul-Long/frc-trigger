fcf1d5950c1db346a69f6a3b0af0c3306d226f13
Paul <baolong.hou@sumscope.com>
2019-02-15 16:33:54 +0800

    update

--- src/Trigger.tsx
+++ src/Trigger.tsx
@@ -1,8 +1,14 @@
 import React from 'react';
 
-interface ITriggerProps {}
+interface ITriggerProps {
+  prefixCls: string;
+}
 
 class Trigger extends React.Component<ITriggerProps> {
+  public static defaultProps = {
+    prefixCls: 'frc-trigger'
+  };
+
   render() {
     return <div />;
   }
